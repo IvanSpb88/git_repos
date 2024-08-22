798206324b3588bff714f4ec1c40f1f684aeac62
IvanSpb88 <ntgp2017@mail.ru>
2024-08-22 22:35:04 +0300

    HW8

--- skypro_python_homeworks/lesson_8/Test/x_clients/test_x_clients.py
+++ skypro_python_homeworks/lesson_8/Test/x_clients/test_x_clients.py
@@ -40,95 +40,93 @@ def test_add_employer(get_token):
     assert info.json()['id'] == new_employer_id
     assert info.status_code == 200
     
-    def test_add_employer_without_token():
-        com_id = company.last_active_company_id()
-        token = ""
-        body_employer = {
-            'id': 0,
-            'firstName': 'Ivan',
-            'lastName': 'Petrov',
-            'middleName': 'string',
-            'companyId': com_id,
-            'email': 'test@mail.ru',
-            'url': 'string',
-            'phone': 'string',
-            'birthday': '2024-05-16T11:02:45.622Z',
-            'isActive': 'true' 
-        }
-        new_employer = employer.add_new(token, body_employer)
-        assert new_employer['message'] == 'Unauthorized'
+def test_add_employer_without_token():
+    com_id = company.last_active_company_id()
+    token = ""
+    body_employer = {
+        'id': 0,
+        'firstName': 'Ivan',
+        'lastName': 'Petrov',
+        'middleName': 'string',
+        'companyId': com_id,
+        'email': 'test@mail.ru',
+        'url': 'string',
+        'phone': 'string',
+        'birthday': '2024-05-16T11:02:45.622Z',
+        'isActive': 'true' 
+    }
+    new_employer = employer.add_new(token, body_employer)
+    assert new_employer['message'] == 'Unauthorized'
         
-        def test_add_employer_without_body(get_token):
-            token = str(get_token)
-            com_id = company.last_active_company_id()
-            body_employer = {}
-            new_employer = employer.add_new(token, body_employer)
-            assert new_employer['message'] == 'Internal server error'
+def test_add_employer_without_body(get_token):
+    token = str(get_token)
+    com_id = company.last_active_company_id()
+    body_employer = {}
+    new_employer = employer.add_new(token, body_employer)
+    assert new_employer['message'] == 'Internal server error'
             
-        def test_get_employer():
-            com_id = company.last_active_company_id()
-            list_employers = employer.get_list(com_id)
-            assert isinstance(list_employers, list)
+def test_get_employer():
+    com_id = company.last_active_company_id()
+    list_employers = employer.get_list(com_id)
+    assert isinstance(list_employers, list)
             
-        def test_get_list_employers_missing_company_id():
-            try:
-                employer.get_list()
-            except TypeError as e:
-                assert str(
-                    e) == "Employer.get_list() missing 1 required positional argument: 'company_id'"
+def test_get_list_employers_missing_company_id():
+    try:
+        employer.get_list()
+    except TypeError as e:
+        assert str(
+            e) == "Employer.get_list() missing 1 required positional argument: 'company_id'"
                 
-        def test_get_list_employers_invalid_company_id():
-            try:
-                employer.get_list('')
-            except TypeError as e:
-                assert str(
-                    e) == "Employer.get_list() missing 1 required positional argument: 'company_id'"
+def test_get_list_employers_invalid_company_id():
+    try:
+        employer.get_list('')
+    except TypeError as e:
+        assert str(
+            e) == "Employer.get_list() missing 1 required positional argument: 'company_id'"
                 
-        def test_get_info_new_employers_missing_employer_id():
-            try:
-                employer.get_info()
-            except TypeError as e:
-                assert str(
-                    e) == "Employer.get_info() missing 1 required positional argument: 'employee_id'"
+def test_get_info_new_employers_missing_employer_id():
+    try:
+        employer.get_info()
+    except TypeError as e:
+        assert str(
+            e) == "Employer.get_info() missing 1 required positional argument: 'employee_id'"
         
-        def test_get_change_employer_info(get_token):
-            token = str(get_token)
-            com_id = company.last_active_company_id()
-            body_employer = {
-                'id': 0,
-                'firstName': 'Ivan',
-                'lastName': 'Petrov',
-                'middleName': 'string',
-                'companyId': com_id,
-                'email': 'test@mail.ru',
-                'url': 'string',
-                'phone': 'string',
-                'birthday': '2024-05-16T11:02:45.622Z',
-                'isActive': 'true' 
-            }
-            just_employer = employer.add_new(token, body_employer)
-            id = just_employer['id']
-            body_change_employer = {
-                'lastName': 'Pupkin',
-                'email': 'test2@mail.ru',
-                'url': 'string',
-                'phone': 'string',
-                'isActive': 'true'
-            }
+def test_get_change_employer_info(get_token):
+    token = str(get_token)
+    com_id = company.last_active_company_id()
+    body_employer = {
+        'id': 0,
+        'firstName': 'Ivan',
+        'lastName': 'Petrov',
+        'middleName': 'string',
+        'companyId': com_id,
+        'email': 'test@mail.ru',
+        'url': 'string',
+        'phone': 'string',
+        'birthday': '2024-05-16T11:02:45.622Z',
+        'isActive': 'true' 
+    }
+    just_employer = employer.add_new(token, body_employer)
+    id = just_employer['id']
+    body_change_employer = {
+        'lastName': 'Pupkin',
+        'email': 'test2@mail.ru',
+        'url': 'string',
+        'phone': 'string',
+        'isActive': 'true'
+    }
             
-            employer_changed = employer.change_info(token, id, body_change_employer)
-            assert employer_changed.status_code == 200
-            
-            assert id == employer_changed.json()['id']
-            
-            assert (employer_changed.json()["email"]
+    employer_changed = employer.change_info(token, id, body_change_employer)
+    assert employer_changed.status_code == 200
+    assert id == employer_changed.json()['id']
+    assert (employer_changed.json()["email"]
                     ) == body_change_employer.get("email")
             
-        def test_employers_missing_id_and_token():
-            try:
-                employer.change_info()
-            except TypeError as e:
-                assert str(
-                    e) == "Employer.change_info() missing 3 requiredpositional arguments: 'token', 'employee_id', and 'body'"
+def test_employers_missing_id_and_token():
+    try:
+        employer.change_info()
+    except TypeError as e:
+        assert str(
+            e) == "Employer.change_info() missing 3 required positional arguments: 'token', 'employee_id', and 'body'"
             
         
